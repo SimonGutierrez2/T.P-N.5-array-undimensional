657a2e2b6457f2c318a50b5e072dfa8239710d74
SimonGutierrez2 <135912274+SimonGutierrez2@users.noreply.github.com>
2023-10-30 16:45:08 -0300

    Add files via upload

---
+++ datos/java.js
@@ -0,0 +1,8 @@
+function añadir(){
+
+    n1 =document.getElementById("num1").value
+    n2 =document.getElementById("num2").value
+    suma=n1+n2
+    
+    alert("la suma de" + n1 + "+" + n2 +"es:" + suma )
+    }
